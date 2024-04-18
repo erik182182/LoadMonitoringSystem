d251a79481ab1353373f55b35a4504c405fa636c
erik182182 <erik182182@yandex.ru>
2024-04-18 12:34:40 +0300

    Добавлены стили, html, js-файлы. Сконфигурирован фреймворк Spring. Добавлено отображение главной страницы системы.

---
+++ src/main/java/ru/erik182182/load/LoadMonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.erik182182.load;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadMonitoringSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadMonitoringSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/erik182182/load/LoadMonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.erik182182.load;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadMonitoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
